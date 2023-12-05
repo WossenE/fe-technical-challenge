a0528492795d58da1d5cb1cd5f23c62860145e97
WossenE <w.hailemariam11@gmail.com>
2023-12-05 21:02:32 +0100

    Fullfilled all requirements

--- src/MoneyInput/MoneyInput.stories.tsx
+++ src/MoneyInput/MoneyInput.stories.tsx
@@ -1,28 +1,32 @@
-import { Meta } from '@storybook/react'
 import MoneyInput, { MoneyInputProps } from './MoneyInput';
 
+import { Meta, Story } from '@storybook/react';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/MoneyInput',
   component: MoneyInput,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {},
-}satisfies Meta<typeof MoneyInput>
+  // argTypes: {
+  //   borderColor: { control: 'color' },
+  //   borderSize: { control: 'text' },
+  //   inputColor: { control: 'color' },
+  // },
+} as Meta<MoneyInputProps>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-const Template: React.FC<MoneyInputProps> = (args) => <MoneyInput {...args} />;
+const Template: Story<MoneyInputProps> = (args) => <MoneyInput {...args} />;
+
 export const Default = Template.bind({});
 Default.args = {
   locale: 'eu',
+  label: 'Label',
   value: 0,
   disabled: false,
   onChange: (value) => console.log('onChange', value),
   onBlur: (value) => console.log('onBlur', value),
+  // borderColor: '#000',
+  // borderSize: '1px',
+  // inputColor: '#000',
 };
